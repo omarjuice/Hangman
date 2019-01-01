6407f92f782c01537b01c8168b73f8a95b39ef38
Omar Juice <ojameer1@gmail.com>
2019-01-01 13:46:39 -0500

    Updates

---
+++ precache-manifest.b5320cb49b0b199de4d9d9c7ac209a91.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "ae1caf0167b4b39de989",
+    "url": "/Hangman/static/js/runtime~main.ae1caf01.js"
+  },
+  {
+    "revision": "3ea4d42a8a2e23bf3d9a",
+    "url": "/Hangman/static/js/main.3ea4d42a.chunk.js"
+  },
+  {
+    "revision": "5999a27d4c930c812b6f",
+    "url": "/Hangman/static/js/1.5999a27d.chunk.js"
+  },
+  {
+    "revision": "5bf0180aa8b3cc1124ea9f5f355af2dc",
+    "url": "/Hangman/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.e69e266d8427312002be4e45938d0ed5.js"
+  "/Hangman/precache-manifest.b5320cb49b0b199de4d9d9c7ac209a91.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/Hangman/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
