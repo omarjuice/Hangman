f832799f8cd2200367b96b682e7c23e36b15721b
Omar Juice <ojameer1@gmail.com>
2019-01-01 14:05:05 -0500

    Updates

--- precache-manifest.89cd7d5183c5c39a12384eb5ebdab6fb.js
+++ precache-manifest.89cd7d5183c5c39a12384eb5ebdab6fb.js
@@ -4,15 +4,15 @@ self.__precacheManifest = [
     "url": "/Hangman/static/js/runtime~main.ae1caf01.js"
   },
   {
-    "revision": "24c5880a47efa7a3ffe5",
-    "url": "/Hangman/static/js/main.24c5880a.chunk.js"
+    "revision": "437b20c4392d7c60dfa7",
+    "url": "/Hangman/static/js/main.437b20c4.chunk.js"
   },
   {
     "revision": "5999a27d4c930c812b6f",
     "url": "/Hangman/static/js/1.5999a27d.chunk.js"
   },
   {
-    "revision": "e319bcea4af987e2fb7ec7f44cdd8811",
+    "revision": "bac6c37e4a7579abfc27057973fd791a",
     "url": "/Hangman/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/Hangman/precache-manifest.d539ea6e5612c5437ff15b50e539f6d0.js"
+  "/Hangman/precache-manifest.89cd7d5183c5c39a12384eb5ebdab6fb.js"
 );
 
 workbox.clientsClaim();
